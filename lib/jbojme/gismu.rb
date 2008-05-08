86b46e3e7d6235d9b0f1ac71c36ed92c2d1095dd
Dag Odenhall <dag.odenhall@gmail.com>
2008-05-08 10:28:55 +0200

    oops

--- lib/jbojme/gismu.rb
+++ lib/jbojme/gismu.rb
@@ -8,7 +8,7 @@ module Jbojme
       data = GISMU_LIST.select {|g| g[0][0] == gismu.to_s.downcase }[0]
       @gismu = data[0][0]
       @rafsi = data[0][1..-1]
-      @smuni = data[2]
+      @smuni = data[1]
       @skicu = data[3]
     end
   end
