c8ec119b16175ea1631c4f7eb660a8f73a95a9b5
Dag Odenhall <dag.odenhall@gmail.com>
2008-05-09 03:21:50 +0200

    No need those nomore

--- lib/jbojme/parse_tree.rb
+++ lib/jbojme/parse_tree.rb
@@ -12,16 +12,12 @@ module Jbojme
       tree = text.split(/[\.\s]+/).map do |token|
         case token
         when /^(?:#@v(?:'?#@v)?|#@c#@v(?:'?#@v)?)+$/
-          [:cmavo, token]
           Cmavo.new(token)
         when /#@c$/
-          [:cmevla, token]
           Cmevla.new(token)
         when /^#@c(?:#@c#@v#@c|#@v#@c#@c)#@v$/
-          [:gismu, token]
           Gismu.new(token)
         when /^#@c.*#@v$/
-          [:lujvo, token]
           Lujvo.new(token)
         end
       end
